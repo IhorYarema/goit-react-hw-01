707649b6275adb399fd9aa37ea61a58a6951840f
Ihor <u96ok69@gmail.com>
2025-04-13 18:23:52 +0300

    fix for  vercel

--- src/components/FriendList/Friendlist.jsx
+++ src/components/FriendList/Friendlist.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styles from './Friendlist.module.css';
+import styles from './FriendList.module.css';
 import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
