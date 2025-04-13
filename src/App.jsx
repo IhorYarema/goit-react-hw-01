2ac8f2887897647ebb0032c37612f5a17410da19
Ihor <u96ok69@gmail.com>
2025-04-13 18:08:50 +0300

    make table task 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import Profile from './components/Profile/Profile';
-import FriendList from './components/Friendlist/Friendlist';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
 import userData from './userData.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => {
   return (
@@ -14,6 +18,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
